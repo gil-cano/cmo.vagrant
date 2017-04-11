7be3968ec7c2dd92a6bf471022ddaef819268d26
gil-cano <gil.cano@gmail.com>
2017-04-11 12:03:56 -0500

    provisiion nginx and php

--- LEMP.sh
+++ LEMP.sh
@@ -4,9 +4,8 @@
 # apt-key add nginx_signing.key
 # echo "deb http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list
 # echo "deb-src http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list
-# apt-get update
-# apt-get install -y nginx
 
+echo "Provisioning virtual machine..."
 apt-get update
 
 # Install the nginx web server
@@ -15,43 +14,12 @@ apt-get install -y nginx
 # Install PHP fastCGI process manager
 apt-get install -y php5-fpm
 
+# Make fpm more secure
+sed -i -e 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
+service php5-fpm restart
+
 # Configure nginx to use the PHP Processor
-cat << 'EOF' > /etc/nginx/sites-available/default
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        # Add index.php to the list if you are using PHP
-        index index.php index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-        }
-
-        # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-        location ~ \.php$ {
-                include snippets/fastcgi-php.conf;
-                # With php5-cgi alone:
-                #fastcgi_pass 127.0.0.1:9000;
-                # With php5-fpm:
-                fastcgi_pass unix:/var/run/php5-fpm.sock;
-        }
-
-        # deny access to .htaccess files, if Apache's document root
-        # concurs with nginx's one
-        location ~ /\.ht {
-                deny all;
-        }
-}
-EOF
-
-# Restart servers
+cp /vagrant/config/nginx_vhost /etc/nginx/sites-available/default
 service nginx reload
 
 # Create a PHP file to test configuration
---
+++ README.rst
@@ -0,0 +1,68 @@
+RailsDev.Vagrant
+================
+
+This is a kit for setting up a development enviroment for LEMP stack (Linux, Nginx, MySQL, PHP) in a hosted virtual machine.
+
+Installation
+------------
+
+1. Install `VirtualBox <https://www.virtualbox.org>`_
+
+2. Install `Vagrant <http://www.vagrantup.com>`_
+
+3. Clone the repository::
+
+    $ git clone  https://github.com/gil-cano/cmo.vagrant.git
+
+4. Change directory into the cmo.vagrant directory. Run "vagrant up"::
+
+    $ cd cmo.vagrant
+    $ vagrant up
+
+This is going to download a virtual box kit, download and install `Nginx <https://www.nginx.com/>`_, `PHP <http://php.net/>`_, `MySQL <https://www.mysql.com/>`_, and set up some convenience scripts.
+
+Using the Vagrant-installed VirtualBox
+--------------------------------------
+
+To you get a command prompt on your "guest" machine use the command::
+
+    $ vagrant ssh
+
+You may now start and stop the virtual machine by issuing command in the same directory::
+
+    $ vagrant suspend
+
+stops the virtual machine, saving an image of its state so that you may later restart with::
+
+    $ vagrant resume
+
+stop virtual machine::
+
+    $ vagrant halt
+
+start virtual machine::
+
+    $ vagrant reload
+
+Finally, you may remove the VirtualBox (deleting its image) with the command::
+
+    $ vagrant destroy
+
+
+Test Configuration
+------------------
+
+We can test it to validate that Nginx can correctly hand .php files off to our PHP processor.
+
+In your web browser open the page::
+
+    http://localhost:8088/info.php
+
+
+  # apt-get install -y mysql-server
+  # php5-mysql
+
+References
+----------
+
+`How To Install Linux, Nginx, MySQL, PHP (LEMP Stack) on Debian 8 <https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-on-debian-8>`_
